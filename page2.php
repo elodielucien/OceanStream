f3aa48791afe8a72acd1814ff8befa79955654d3
elodielucien <elodielucien@gmail.com>
2019-01-28 20:06:18 +0100

    premiersEssaisPHP

---
+++ page2.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" /> <!--indique le type d'encodage utilisé dans le fichier : .html -->
+        <title>OceanStream</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+
+    <body>
+
+
+    <div class ="Menu">
+        <nav class ="elements"> <!-- pour le menu du site -->
+                <div> <strong>Menu</strong> </div>
+                <div><a href = "page1.html" > Accueil </a> </div>
+                <div><a href = "Contenu/page2.html"> Page 2 </a> </div>
+
+        </nav>
+    </div> 
+
+    <h1>Bienvenue sur la page 2</h1> <br/>
+
+    <h2 id=AncrePage2>AncrePage2 </h2>
+
+    <caption>Les poissons</caption> <!--titre du tableau -->
+
+     <table class= "table" >
+                <tr>   <!-- première ligne-->
+                    <th>  <!-- début première cellule. La balise th indique qu'il s'agit d'une en-tête du tableau -->
+                        Nom
+                    </th> <!-- fin première cellule -->
+
+                    <th> Type </th>
+
+                    <th>Race </th>
+                </tr>  <!--fin première ligne-->
+
+                <tr>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+
+                <tr>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+
+                </tr>
+
+                <tr>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+
+            </table>
+    
+    </body>
+</html>
